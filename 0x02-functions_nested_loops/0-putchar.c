b0ebdf2a4a73c73dc0b753ab69bf8d796f7115e4
actrx <actrx@yandex.com>
2022-09-13 19:55:54 +0100

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,19 @@
 
 /**
  * main - Entry point
- * Description: print holberton followed by a new line
+ * Description: print _putchar followed by a new line
  * Return: 0 (success)
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
