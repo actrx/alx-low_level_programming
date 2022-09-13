9f2947aef765831ccb9ab062c0f2298eb43c9c8b
actrx <actrx@yandex.com>
2022-09-13 19:33:17 +0100

    functions & nested loops

---
+++ 0x00-hello_world/putff.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main-void
+ * init string
+ * puts char
+ * Always return 0
+ */
+
+int main(void)
+{
+	char a[]="janbooldoer";
+
+	puts(a);
+
+	return(0)
+}
