60817dc8129ca6f438d61de3612d830dce67660a
actrx <actrx@yandex.com>
2022-10-10 21:19:34 +0100

    A door is what a dog is perpetually on the

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,11 +14,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
+	if (ptr->name == NULL || ptr->owner == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	return (ptr);
 }
