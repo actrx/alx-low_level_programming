2ba95a332547cbe3cecdb74692c3003b7e178aa9
actrx <actrx@yandex.com>
2022-10-10 18:02:08 +0100

    dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - check the code
+ * @d: pointer to struct dog
+ * @name: pointer to name
+ * @owner: pointer to char
+ * @age: pointer to int
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
