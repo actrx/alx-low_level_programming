e4504436abec85560fbc2eb8103a584544532b7e
actrx <actrx@yandex.com>
2022-10-14 02:51:34 +0100

    VARIADIC_FUNCTIONS

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,14 @@
 #ifndef __VARIADIC_FUNCTIONS_H_
 #define __VARIADIC_FUNCTIONS_H_
-
+typedef struct print_types
+{
+    char arg;
+    void (*print_func)(va_list);
+} p_type;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_all(const char *const format, ...);
 
 #endif /* __VARIADIC_FUNCTIONS_H_ */
-
